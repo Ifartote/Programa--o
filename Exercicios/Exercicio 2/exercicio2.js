7d25d15ecac84c50d1d6d5a34a72b4c74c2d9d15
GRS Teste <ifartote2020@outlook.com.com>
2025-04-08 22:05:32 -0300

    Parte5.3

---
+++ Exercicios/Exercicio 2/exercicio2.js
@@ -0,0 +1,35 @@
+//A variavel Let não aceita mais de uma declaração, então se a gente colocar:
+//let pessoa = "Igor";
+//let pessoa = "João";
+//Vai aparecer uma mensagem de erro, pois ela pode ser declarada apenas uma vez.
+
+//Já no VAR, podemos redeclarar ela, que não irá dar problema.
+
+//CONST ela não muda, ent uma vez declarada não vai ser mudada, nem mesmo se utilizar um scopo 
+
+
+//Funções
+
+//function soma(valor1, valor2) {
+//  return valor1 + valor2;
+//}
+
+
+//var total= soma(10, 23);
+//alert(soma);
+
+//function alertaHello() {
+  //  alert("Olá pessoal");
+//}
+
+//function paraCelsius(valorFahrenheit){
+//    return(5/9) * (valorFahrenheit - 32);
+//}
+
+//var x = paraCelsius(77);
+
+//alert("A temperadura é de " + x + " graus celcius");
+
+
+//Eventos
+
