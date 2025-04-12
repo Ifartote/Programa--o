e8f8e261cb4e4c23877c39e4c169589ac9cf161a
GRS Teste <ifartote2020@outlook.com.com>
2025-04-12 00:08:55 -0300

    Lista de Tarefas, erro

--- Projetos/Projeto 1/projeto1.js
+++ Projetos/Projeto 1/projeto1.js
@@ -1,3 +1,4 @@
+let contador = 0;
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
 let main = document.getElementById('areaLista');
@@ -6,26 +7,63 @@ function addTarefa() {
     //Pegar o valor digitado no INPUT
     let valorInput = input.value;
 
-  // Se não for vazio, nem nulo, nem indefinido
-  if((valorInput !=="") && (valorInput!==null) && (valorInput!==undefined)){
-    let novoItem = `<div class="item">
-            <div class="item-icone">
-                <i class="fa-regular fa-circle"></i>
+    // Se não for vazio, nem nulo, nem indefinido
+    if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
+        ++contador;
 
+        let novoItem = `<div id="${contador}" class="item">
+            <div onclick ="marcarTarefa(${contador})" class="item-icone">
+                <i id="icone_${contador}" class="fa-regular fa-circle"></i>
             </div>
-            <div class="item-nome">
-                Teste de Tarefa
+            <div onclick="marcarTarefa(${contador})" class="item-nome">
+                ${valorInput}
             </div>
             <div class="item-botao">
-                <button class="delete"><i class="fa-solid fa-trash"></i>Deletar</button>
-
+                <button onclick="deletar(${contador})" class="delete"><i class="fa-solid fa-trash"></i>Deletar</button>
+                </div>
             </div>`;
+        // Adicionar novo item no MAIN
+        main.innerHTML += novoItem;
+        // Zerar os campinhos
+        input.value = "";
+        input.focus();
 
-            main.innerHTML += novoItem;
-
-
-
-  }
-
+    }
 
 }
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute("class");
+    console.log(classe);
+
+    if (classe == "item") {
+        item.classList.add("clicando")
+
+        var icone = document.getElementById("icone_" + id);
+        icone.classList.remove("fa-circle");
+        icone.classList.add("fa-circle-check");
+
+        item.parentNode.appendChild(item);
+    } else {
+        item.classList.remove("clicado");
+
+        var icone = document.getElementById("icone_" + id);
+        icone.classList.remove("fa-circle-check");
+        icone.classList.add("fa-circle");
+
+    }
+
+}
+
+input.addEventListener("keyup", function (event) {
+    //Se teclou enter (13)
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        btnAdd.click();
+    }
+});
