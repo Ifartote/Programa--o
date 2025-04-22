05cd2bbaadd755e92904a84108b2ca23cd5f386a
GRS Teste <ifartote2020@outlook.com.com>
2025-04-21 22:52:44 -0300

    Projeto 4

--- Projetos/Projeto 3/projeto3.js
+++ Projetos/Projeto 3/projeto3.js
@@ -1,4 +1,6 @@
-function filtrarTabela(){
+let dados = [];
+
+function filtrarTabela() {
 
     var filtroNome = document.getElementById("filtroNome").value.toLowerCase();
     var filtroDepartamento = document.getElementById("filtroDepartamento").value.toLowerCase();
@@ -14,23 +16,41 @@ function filtrarTabela(){
         var salario = linhas[i].cells[3].textContent.toLowerCase();
         var tempo = linhas[i].cells[4].textContent.toLowerCase();
 
-        linhas[i].style.display = 
-        (nome.includes(filtroNome) || filtroNome === "") &&
-        (departamento.includes(filtroDepartamento) || filtroDepartamento === "") &&
-        (cargo.includes(filtroCargo) || filtroCargo === "") &&
-        (salario.includes(filtroSalario) || filtroSalario === "") &&
-        (tempo.includes(filtroTempo) || filtroTempo === "") 
+        linhas[i].style.display =
+            (nome.includes(filtroNome) || filtroNome === "") &&
+                (departamento.includes(filtroDepartamento) || filtroDepartamento === "") &&
+                (cargo.includes(filtroCargo) || filtroCargo === "") &&
+                (salario.includes(filtroSalario) || filtroSalario === "") &&
+                (tempo.includes(filtroTempo) || filtroTempo === "")
 
-        ? "" : "none";
+                ? "" : "none";
 
 
     }
 }
-function formatarSalario(salario){
-    return parseFloat(salario).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+function formatarSalario(salario) {
+    return parseFloat(salario).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
 
-function carregarDados(dados){
+const adicionar = () => {
+    const modal = document.querySelector("#modal");
+    modal.style.display == "flex" ? modal.style.display = "none" : modal.style.display = "flex";
+}
+
+const enviarDados = () => {
+    const novoUsuario = {
+        Nome: nomeInput.value,
+        Departamento: departamentoInput.value,
+        Cargo: cargoInput.value,
+        Salário: salarioInput.value,
+        "Tempo de Empresa (anos)": tempoInput.value
+    }
+    dados.push(novoUsuario);
+    atualizarArquivo();
+}
+
+function carregarDados() {
+
 
     var corpoTabela = document.getElementById("corpoTabela");
     dados.forEach(funcionario => {
@@ -44,20 +64,38 @@ function carregarDados(dados){
             <td>${funcionario["Tempo de Empresa (anos)"]}</td>
              `;
 
-            corpoTabela.appendChild(linha);
+        corpoTabela.appendChild(linha);
     });
 }
+fetch("https://opensheet.elk.sh/https://docs.google.com/spreadsheets/d/1ufZZv-_MwymYxnmrW0jm72_Oq15RvU0LhA_WNYF6hmQ/edit?usp=sharing")
+.then(res => res.json())
+.then(data => {
+  const tbody = document.querySelector("#tabela tbody");
+  data.forEach(row => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td>${row.Nome}</td><td>${row.Idade}</td>`;
+    tbody.appendChild(tr);
+  });
+});
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
 
     fetch('funcionarios.xlsx')
-    .then(response => response.arrayBuffer())
-    .then(data => {
-        var workbook = XLSX.read(data, {type: 'array'});
-        var primeiraSheet = workbook.Sheets[workbook.SheetNames[0]];
-        var dadosJSON = XLSX.utils.sheet_to_json(primeiraSheet);
-        carregarDados(dadosJSON)
-    
-    })
-    .catch(error => console.error('Erro ao carregar os dados:', error));
+        .then(response => response.arrayBuffer())
+        .then(data => {
+            var workbook = XLSX.read(data, { type: 'array' });
+            var primeiraSheet = workbook.Sheets[workbook.SheetNames[0]];
+            var dadosJSON = XLSX.utils.sheet_to_json(primeiraSheet);
+            dados = dadosJSON;
+            carregarDados()
+
+        })
+        .catch(error => console.error('Erro ao carregar os dados:', error));
 });
+
+function exportarParaExcel() {
+    var tabela = document.getElementById("tabelaFuncionarios");
+    var workbook = XLSX.utils.table_to_book(tabela);
+    XLSX.writeFile(workbook, 'funcionarios.xlsx');
+}
